b8b55a069fc96269ca9e5eafcdcab581658586f7
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-29 23:44:59 -0300

    Stage 2/8

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,6 +1,22 @@
 package calculator
 
 fun main() {
-    val numbers = readln().split(" ").map { it.toInt() }
-    println(numbers.sum())
+    while (true) {
+        val inputNumbers = readln()
+        val numbers = mutableListOf<Int>()
+
+        if (inputNumbers == "/exit") {
+            println("Bye!")
+            break
+        } else if (inputNumbers.isNullOrBlank()) continue
+        else {
+            inputNumbers.split(" ").map { numbers.add(it.toInt()) }
+        }
+
+        if (numbers.size == 1) println(numbers[0])
+        else {
+            val (x, y) = numbers
+            println(x + y)
+        }
+    }
 }
