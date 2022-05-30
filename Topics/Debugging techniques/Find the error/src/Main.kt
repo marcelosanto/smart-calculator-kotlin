acb3aacff476ddda8efb5eb743ce2c6cfb012f4f
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-29 23:34:50 -0300

    Stage 1/8: 2+2

--- Topics/Debugging techniques/Find the error/src/Main.kt
+++ Topics/Debugging techniques/Find the error/src/Main.kt
@@ -1,18 +1,19 @@
-import java.util.*
-
 fun swapInts(ints: IntArray): IntArray {
     return intArrayOf(ints[1], ints[0])
 }
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    while (scanner.hasNextLine()) {
-        var ints = intArrayOf(
-            scanner.nextLine().toInt(),
-            scanner.nextLine().toInt(),
-        )
-        swapInts(ints)
-        println(ints[1])
-        println(ints[0])
-    }
+    //val scanner = Scanner(System.`in`)
+//    while (scanner.hasNextLine()) {
+//        var ints = intArrayOf(
+//            scanner.nextLine().toInt(),
+//            scanner.nextLine().toInt(),
+//        )
+//        swapInts(ints)
+//        println(ints[1])
+//        println(ints[0])
+//    }
+
+    val x1 = setOf(1, 2, 3, 5)
+    println(x1)
 }
